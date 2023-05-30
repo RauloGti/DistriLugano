3235ab001ceccfea01d8e9cbf4b539d1d3f849c5
RauloGti <raulogtis@gmail.com>
2023-05-30 15:18:05 -0300

    se realizo la pantalla del login , html

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+//formulario Reactivo, para utilizar los datos login.import {formBuilder , validator} 
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  form = this.formBuilder.group({
+    email: ['',[Validators.email, Validators.required]],
+    password: ['',[Validators.required]],
+  })
+
+  constructor(
+    private formBuilder : FormBuilder
+  ) { }
 
   ngOnInit() {
   }
