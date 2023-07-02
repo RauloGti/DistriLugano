5941dd1b3cfc3d77e2a99c539f0154d8d1178cb1
Ezequiel <cruz.ezequiel.p@gmail.com>
2023-07-02 14:04:03 -0300

    Se modificaron los pedidos

--- src/app/crear-pedido/crear-pedido.page.ts
+++ src/app/crear-pedido/crear-pedido.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from './../services/auth.service';
 import { Router } from '@angular/router';
-import { Firestore, collection, getDocs, getFirestore } from '@angular/fire/firestore';
+import { Firestore, collection, getDocs, getFirestore ,getDoc,doc} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-crear-pedido',
@@ -11,10 +11,13 @@ import { Firestore, collection, getDocs, getFirestore } from '@angular/fire/fire
 })
 export class CrearPedidoPage implements OnInit {
   form: FormGroup = new FormGroup({});
-  productos: any[] = [];
+  productos: any = [];
   clientes: any[] = [];
   repartidores: any[] = [];
   datos:any;
+
+  products :any
+
   constructor(
     private formBuilder: FormBuilder,
     private auth: AuthService,
@@ -36,23 +39,18 @@ export class CrearPedidoPage implements OnInit {
     this.obtenerRepartidores();
   }
 
-  async obtenerProductos() {
+  async obtenerProductos(){
+    const db = getFirestore();
+    const docRef = doc(db, "Productos", "IdProductos");
+    const docSnap = await getDoc(docRef);
+    docSnap.data();
     try {
-      const db = getFirestore();
-      const collectionRef = collection(db, 'IdProductos');
-      const querySnapshot = await getDocs(collectionRef);
-      this.productos = [];
-      querySnapshot.forEach((doc) => {
-        const producto = {
-          id: doc.id,
-          
-          // Otros campos del producto
-          // ...
-        };
-        this.productos.push(producto);
-      });
-    } catch (error) {
-      console.log('Error al obtener los productos:', error);
+      const docSnap = await getDoc(docRef);
+      let datos : any = docSnap.data()
+      this.products = datos.productos
+    } 
+    catch(error) {
+      console.log(error)
     }
   }
 
@@ -99,4 +97,126 @@ export class CrearPedidoPage implements OnInit {
 
   // Resto del código de la página
   // ...
+  
+
+  agregarProducto1(){
+    this.pedido1 = this.pedido1 +1
+  }
+
+  restarProducto1(){
+    this.pedido1 = this.pedido1 -1
+    if(this.pedido1==-1){
+      this.agregarProducto1()
+    }
+  }
+
+  agregarProducto2(){
+    this.pedido2 = this.pedido2 +1
+  }
+
+  restarProducto2(){
+    this.pedido2 = this.pedido2 -1
+    if(this.pedido2==-1){
+      this.agregarProducto2()
+    }
+  }
+
+  agregarProducto3(){
+  this.pedido3 = this.pedido3 +1
+  }
+  
+  restarProducto3(){
+    this.pedido3 = this.pedido3 -1
+    if(this.pedido3==-1){
+      this.agregarProducto3()
+    }
+  }
+
+
+  agregarProducto4(){
+    this.pedido4 = this.pedido4 +1
+  }
+  
+  restarProducto4(){
+    this.pedido4 = this.pedido4 -1
+    if(this.pedido4==-1){
+      this.agregarProducto4()
+    }
+  }
+
+  agregarProducto5(){
+  this.pedido5 = this.pedido5 +1
+  }
+  
+  restarProducto5(){
+    this.pedido5 = this.pedido5 -1
+    if(this.pedido5==-1){
+      this.agregarProducto5()
+    }
+  }
+
+  agregarProducto6(){
+  this.pedido6 = this.pedido6 +1
+  }
+
+  restarProducto6(){
+    this.pedido6 = this.pedido1 -6
+    if(this.pedido6==-1){
+      this.agregarProducto6()
+    }
+  }
+
+  guardarProductos(){
+    let listaProductos :any
+
+    listaProductos = [
+      {
+        id:0,
+        producto : "Clásicas 100gr",
+        cantidad :this.products[0].cantidad - this.pedido1
+      },
+      {
+        id:1,
+        producto : "Clásicas 150gr",
+        cantidad :this.products[1].cantidad - this.pedido2
+      },
+      {
+        id:2,
+        producto : "Clásicas 475gr",
+        cantidad :this.products[2].cantidad - this.pedido3
+      },
+      {
+        id:3,
+        producto : "Onduladas 100gr",
+        cantidad :this.products[3].cantidad - this.pedido4
+      },
+      {
+        id:4,
+        producto : "Onduladas 180gr",
+        cantidad :this.products[4].cantidad - this.pedido5
+      },
+      {
+        id:5,
+        producto : "Onduladas 475gr",
+        cantidad :this.products[5].cantidad - this.pedido6
+      }
+    ]
+
+    console.log(listaProductos)
+    
+
+  }
+
+
+ pedido1 :any = 0
+ pedido2 :any = 0
+ pedido3 :any = 0
+ pedido4 :any = 0
+ pedido5 :any = 0
+ pedido6 :any = 0
+
+
+  cantidad(id :any){
+   
+  }
 }
